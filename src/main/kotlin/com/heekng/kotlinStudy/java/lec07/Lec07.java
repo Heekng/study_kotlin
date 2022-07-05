cc141cc3862b9f719c564a0e97333593b4819f8d
heekng <rhkd1769@gmail.com>
2022-07-06 01:57:01 +0900

    예외

---
+++ src/main/kotlin/com/heekng/kotlinStudy/java/lec07/Lec07.java
@@ -0,0 +1,43 @@
+package com.heekng.kotlinStudy.java.lec07;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Lec07 {
+
+    private int parseIntOrThrow(@NotNull String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(String.format("주어진 %s는 숫자가 아닙니다.", str));
+        }
+    }
+
+    private Integer parseIntOrThrowV2(@NotNull String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public void readFile() throws IOException {
+        File currentFile = new File(",");
+        File file = new File(currentFile.getAbsolutePath() + "/a.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        System.out.println(reader.readLine());
+        reader.close();
+    }
+
+    public void readFileV2(String path) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            System.out.println(reader.readLine());
+        }
+    }
+
+}
+
---
+++ src/main/kotlin/com/heekng/kotlinStudy/kotlin/lec07/Lec07_2.kt
@@ -0,0 +1,6 @@
+package com.heekng.kotlinStudy.kotlin.lec07
+
+fun main() {
+    val filePrinter = FilePrinter()
+    filePrinter.readFile()
+}
